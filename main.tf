9d70c901ce6aa131dc5788bf844816eca3f232da
ptonini <pedro.tonini@gmail.com>
2024-12-31 21:16:14 -0300

    chore: first release

---
+++ main.tf
@@ -0,0 +1,36 @@
+resource "azurerm_public_ip" "this" {
+  name                = "${var.name}-public-ip"
+  resource_group_name = var.rg.name
+  location            = var.rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  sku_tier            = "Regional"
+  zones               = [1, 2, 3]
+  lifecycle {
+    ignore_changes = [
+      tags["business_unit"],
+      tags["environment"],
+      tags["product"],
+      tags["subscription_type"]
+    ]
+  }
+}
+
+resource "azurerm_nat_gateway" "this" {
+  name                = var.name
+  resource_group_name = var.rg.name
+  location            = var.rg.location
+  lifecycle {
+    ignore_changes = [
+      tags["business_unit"],
+      tags["environment"],
+      tags["product"],
+      tags["subscription_type"]
+    ]
+  }
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "this" {
+  nat_gateway_id       = azurerm_nat_gateway.this.id
+  public_ip_address_id = azurerm_public_ip.this.id
+}
